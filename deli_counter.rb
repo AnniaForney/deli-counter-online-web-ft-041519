31eb77078d28ce398dafa45a663ebf9cdf8c1006
Annia Forney <anniaforney@gmail.com>
2019-04-20 19:30:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,14 @@ def line
   return "The line is currently empty."
 end 
 
-def take_a_number(katz_deli) 
-  
+def take_a_number(katz_deli, "Annia") 
+ return katz_deli  
+end 
+
+def take_a_number(katz_deli, "Derek") 
+ return katz_deli  
+end 
+
+def take_a_number(katz_deli, "Penny") 
+ return katz_deli  
 end 
